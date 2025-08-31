179eb860dbd64f88324fdc96fc8866f26e3b3fd1
dawoud <UG_31093938@f-eng.tanta.edu.eg>
2025-08-31 10:18:53 +0300

    POC Routing

--- src/ResponseGenerator.java
+++ src/ResponseGenerator.java
@@ -8,7 +8,31 @@ public class ResponseGenerator {
         switch (path){
             case "/":
                 body="<h1>Welcome to My Java HTTP Server!</h1>" +
-                        "<p>Server is Working </p>";
+                        "<p>Available routes:</p>" +
+                        "<ul>" +
+                        "<li><a href='/about'>About</a></li>" +
+                        "<li><a href='/random'>Random Number</a></li>" +
+                        "<li><a href='/stats'>Server Stats</a></li>" +
+                        "</ul>";
+                break;
+            case "/about":
+                body = "<h1> About This Server</h1>" +
+                        "<p>This is a custom HTTP server built in Java</p>" +
+                        "<p>Features: Multi-threading, routing, and more</p>" ;
+
+                break;
+            case "/random":
+                int randomNum = (int)(Math.random() * 100);
+                body = "<h1>Random Number Generator</h1>" +
+                        "<p>Your lucky number: <strong>" + randomNum + "</strong></p>" +
+                        "<a href='/random'>Generate Another</a>";
+                break;
+            case "/stats":
+                body = "<h1>Server Statistics</h1>" +
+                        "<ul>" +
+                        "<li>Active threads: " + Thread.activeCount() + "</li>" +
+                        "<li>Available processors: " + Runtime.getRuntime().availableProcessors() + "</li>" +
+                        "</ul>";
                 break;
             default:
                 responseHeader = "HTTP/1.1 404 Not Found";
@@ -16,10 +40,13 @@ public class ResponseGenerator {
                         "<p>The page '" + path + "' doesn't exist!</p>" +
                         "<a href='/'>Go back home</a>";
                 break;
+
         }
         return responseHeader + "\n"+
                 "Content-Type: "+contentType+"\n"+
                 "\n"+ // meta-data separator
                 body;
     }
-}
+
+    }
+
